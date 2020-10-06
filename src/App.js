cd89b938c18fa290393c676c536ca2a1d71e4c19
Zahra Azin <ztara.azin@gmail.com>
2020-10-05 21:09:51 -0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import Buttons from "./components/Buttons";
+import SearchResults from "./components/SearchResults";
+import employees from "./employees.json";
+import Container from "./components/Container";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    employees: [],
+    nameSort: "ASC",
+    phoneSort: "ASC",
+    userInput: "",
+    results: []
+  };
+
+
+
+  searchName = (value) => {
+    this.findName(this.state.employees, 0, value);
+  };
+
+  componentDidMount() {
+    this.setState({ employees: employees });
+    this.setState({ results: employees });
+  }
+
+  findName(names, index, letter) {
+
+    var filteredNames = [];
+    names.forEach(thisName => {
+      var checkName = thisName.name;
+      var lowerName = checkName.toLowerCase();
+      if (lowerName.startsWith(letter.toLowerCase(), 0)) {
+        filteredNames.push(thisName);
+        console.log(thisName);
+      }
+    });
+    console.log(filteredNames);
+    this.setState({ results: filteredNames });
+  }
+
+
+
+  ascCompare(a, b) {
+    var nameA = a.name.toUpperCase();
+    var nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  }
+
+  descCompare(a, b) {
+    var nameA = a.name.toUpperCase();
+    var nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return 1;
+    }
+    if (nameA > nameB) {
+      return -1;
+    }
+    return 0;
+  }
+
+  sortByName = () => {
+    let sortedEmployees = this.state.employees;
+    sortedEmployees.sort();
+    
+    var sortOrder = this.state.nameSort;
+    var items = this.state.employees;
+    items.sort(sortOrder === 'ASC' ? this.ascCompare : this.descCompare);
+    console.log(items);
+    if (sortOrder === 'ASC') {
+      sortOrder = 'DESC';
+    } else {
+      sortOrder = 'ASC';
+    }
+    
+    this.setState({
+      employees: sortedEmployees,
+      nameSort: sortOrder
+    })
+  }
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({
+      [name]: value
+    });
+    this.searchName(value);
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Header>Employee Header</Header>
+        <Container>
+         
+          <form className="search">
+            <div className="form-group">
+              <input
+                value={this.state.userInput}
+                name="userInput"
+                type="text"
+                className="form-control"
+                placeholder="Search by name"
+                id="employee"
+                onChange={this.handleInputChange}
+                style={{ width: "30%", margin: "0 auto", marginLeft: "auto", marginRight: "auto" }}
+              />
+
+            </div>
+          </form>
+          <Buttons sort={this.sortByName} />
+          <SearchResults employees={this.state.results} />
+        </Container>
+      </Wrapper>
+    );
+  }
 }
-
 export default App;
