598bfd3e3da88c047168ab1fc6ee51d17cdb182e
Zahra Azin <ztara.azin@gmail.com>
2020-10-05 20:59:11 -0400

    update components

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+  
+import React from "react";
+import "./style.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <h3>Dunder Mifflin Directory</h3>
+            <p>Find who you're looking for</p>
+        </header>
+    );
+}
+
+export default Header;
