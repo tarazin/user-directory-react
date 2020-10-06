f4851fb34f2cb26672caee9b01a9784a29718e92
Zahra Azin <ztara.azin@gmail.com>
2020-10-05 23:38:02 -0400

    Updates

---
+++ precache-manifest.abc8715aa8f672cf0b7f1d8acef41aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c846499338d77ddb31b40c5d0a872718",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47870813ba548bab5dc8",
+    "url": "/static/css/main.a27f2a4f.chunk.css"
+  },
+  {
+    "revision": "d9b35899e26f655c8213",
+    "url": "/static/js/2.2f6e7835.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f6e7835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47870813ba548bab5dc8",
+    "url": "/static/js/main.a1bdade0.chunk.js"
+  },
+  {
+    "revision": "5a658f61801743a1dc6d",
+    "url": "/static/js/runtime-main.794cdbbf.js"
+  }
+]);
